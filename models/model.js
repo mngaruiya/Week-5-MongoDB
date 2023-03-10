359cd1b261cf885f0f0cc390624a51f78c949ec1
Martha Jepkemoi Ngaruiya <113425224+mngaruiya@users.noreply.github.com>
2023-03-10 21:21:27 +0300

    DB model/Schema

---
+++ models/model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// Schema
+const patientSchema = new mongoose.Schema({
+
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: false,
+    },
+    doctor: {
+        type: String,
+        required: true,
+    },
+});
+module.exports = mongoose.model('Team', teamSchema);
